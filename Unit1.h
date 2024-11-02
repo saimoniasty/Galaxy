e26d5daac11d24225cf305e567f26e071e9d3114
saimoniasty <siamon8732@gmail.com>
2024-11-02 15:34:09 +0100

    transparent nie dziala

--- Unit1.h
+++ Unit1.h
@@ -7,10 +7,14 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+        TImage *statek;
+        TImage *Image1;
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);

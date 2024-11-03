37abf44a7c1f68b646b7b828f11de3dee3656a2f
saimoniasty <siamon8732@gmail.com>
2024-11-03 14:30:42 +0100

    jakie podstawowe rzeczy menu start

--- Unit1.cpp
+++ Unit1.cpp
@@ -4,12 +4,14 @@
 #pragma hdrstop
 
 #include "Unit1.h"
+#include "mmsystem.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
 
   int ktory=1;
+  bool gra_rozpoczeta=false;
 
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
@@ -23,16 +25,21 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
       TShiftState Shift)
 {
-   if(Key=='D' || Key=='d' || Key==VK_RIGHT) Prawo->Enabled=true;
-   if(Key=='A' || Key=='a' || Key==VK_LEFT) Lewo->Enabled=true;
+   if(gra_rozpoczeta){
+     if(Key=='D' || Key=='d' || Key==VK_RIGHT) Prawo->Enabled=true;
+     if(Key=='A' || Key=='a' || Key==VK_LEFT) Lewo->Enabled=true;
+     if(Key==VK_SPACE) sndPlaySound("snd/strzal.wav",SND_ASYNC);
+   }
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TForm1::FormKeyUp(TObject *Sender, WORD &Key,
       TShiftState Shift)
 {
-   if(Key=='D' || Key=='d' || Key==VK_RIGHT) Prawo->Enabled=false;
-   if(Key=='A' || Key=='a' || Key==VK_LEFT) Lewo->Enabled=false;
+   if(gra_rozpoczeta){
+     if(Key=='D' || Key=='d' || Key==VK_RIGHT) Prawo->Enabled=false;
+     if(Key=='A' || Key=='a' || Key==VK_LEFT) Lewo->Enabled=false;
+   }
 }
 //---------------------------------------------------------------------------
 
@@ -49,7 +56,7 @@ void __fastcall TForm1::PrawoTimer(TObject *Sender)
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TForm1::statek_ruch1Timer(TObject *Sender)
+void __fastcall TForm1::statek_animacjaTimer(TObject *Sender)
 {
    switch(ktory){
     case 1:
@@ -67,5 +74,13 @@ void __fastcall TForm1::statek_ruch1Timer(TObject *Sender)
 }
 //---------------------------------------------------------------------------
 
-
+void __fastcall TForm1::startClick(TObject *Sender)
+{
+   gra_rozpoczeta=true;
+   tytul->Visible=false;
+   start->Visible=false; high_score->Visible=false; dadada->Visible=false;
+   tlo->Visible=true; tlo_tytul->Visible=false;
+   statek->Visible=true;
+}
+//---------------------------------------------------------------------------
 
